bfbf4ab92fb710264936aa994f6857567a5d6fca
westen0909 <osillojoshua@gmail.com>
2016-08-23 10:47:02 +0800

    Add Comment Function Delete/ Edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
 	belongs_to :user
+	has_many :comments
 
 	has_attached_file :image, styles: { medium: "640x640#", small: "320x320#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
