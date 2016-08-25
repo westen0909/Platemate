4c41b2cc24330dd638c5f330f4fd9771cc24cda3
westen0909 <osillojoshua@gmail.com>
2016-08-26 01:25:52 +0800

    Add Profile Page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  
+  has_attached_file :avatar, styles: { medium: '152x152#' }  
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }  
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }  
 	resources :posts do
 		resources :comments 
 	end
-
+	
+	patch ':user_name/edit', to: 'profiles#update', as: :update_profile  
+	get ':user_name/edit', to: 'profiles#edit', as: :edit_profile 
+	get ':user_name', to: 'profiles#show', as: :profile  
 	root 'posts#index'
 end
