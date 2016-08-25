c7f53116cfe2fbf3d3f572e80584e963c88e62b7
westen0909 <osillojoshua@gmail.com>
2016-08-26 00:58:19 +0800

    Latest

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 	before_action :set_post
 
+
 	def create
 		@comment = @post.comments.build(comment_params)
 		@comment.user_id = current_user.id
