40a2823104274b9f0a038605c8271a7680af0dd8
westen0909 <osillojoshua@gmail.com>
2016-08-26 08:37:36 +0800

    Add style to all

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!, except: [:index, :show]
+	before_action :authenticate_user!
 	before_action :owned_post, only: [:edit, :update, :destroy]
 
 	def index
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 	validates :user_id, presence: true 
+	validates :title, presence: true 
+	validates :price, presence: true 
+	validates :location, presence: true 
+	validates :review,  length: { minimum: 0, maximum: 300 }  
+	validates :image, presence: true
 	has_attached_file :image, styles: { medium: "640x640#", small: "320x320#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
+
